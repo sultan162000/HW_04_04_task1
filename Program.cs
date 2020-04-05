0f790ce9ce0d491a7c11daf0d6871c163246b8e9
Кодиров Султон <sultonkodiro109@gmail.com>
2020-04-05 21:30:56 +0500

    Распуталься

--- Program.cs
+++ Program.cs
@@ -12,11 +12,23 @@ namespace HW_04_04_task2
     }
 
     class Converter{
-        CurrensSomoni cursmn = new CurrensSomoni();
+        public double myBalance = 0;
+        public string myCurrrens = " ";
+
+        public double smn{get;set;}
+        public string smnOfLatin = "Сомони";
+        public double usd{get;set;}
+        public string usdOfLatin = "Доллар";
+        public double eur{get;set;}
+        public string eurOfLatin = "Евро";
+        public double rub{get;set;}
+        public string rubOfLatin = "Рубль";
+
 
         public Converter(double somoni){
-            
-            cursmn.smn = somoni;
+            smn = somoni;
+            myBalance = smn;
+            myCurrrens = smnOfLatin;
             startTheProject();
         }
 
@@ -27,24 +39,90 @@ namespace HW_04_04_task2
             bool check = true;
             while (check)
             {
-                System.Console.WriteLine("1.Конверть валют\n2.Выход");
+                System.Console.WriteLine("1.Показать баланс\n2.Конвертировать\n3.Выход");
                 int x = int.Parse(Console.ReadLine());
                 switch (x)
                 {
                     case 1:
-                    System.Console.WriteLine("1.USD\n2.EURO\n3.RU");
+                    System.Console.WriteLine("Ваш баланс: "+myBalance +" "+myCurrrens);
+                    break;
+
+                    case 2:
+                    bool check1 = true;
+                    while(check1){
+                    System.Console.WriteLine("1.Сомони\n2.USD\n3.EURO\n4.Ru\n5.Назад");
                     int n = int.Parse(Console.ReadLine());
                     switch (n)
                     {
                         case 1:
-                        CurrensUsd curusd = new CurrensUsd(){usd = cursmn.smn/10.23};
-                        System.Console.WriteLine(curusd.usd);
+                        if (myCurrrens == smnOfLatin)
+                        {
+                            System.Console.WriteLine("Невозможно конвертировать");
+                        }else if(myCurrrens == usdOfLatin){
+                            myCurrrens = smnOfLatin;
+                            myBalance = myBalance*10.22;
+                            System.Console.WriteLine(myBalance+" "+myCurrrens);
+                        }else if(myCurrrens == eurOfLatin){
+                            myCurrrens = smnOfLatin;
+                            myBalance = myBalance*11.04;
+                            System.Console.WriteLine(myBalance+" "+myCurrrens);
+                        }else if(myCurrrens == rubOfLatin){
+                            myCurrrens = smnOfLatin;
+                            myBalance = myBalance*0.13;
+                            System.Console.WriteLine(myBalance+" "+myCurrrens);
+                        }
+                        break;
+
+                        case 2:
+                        if (myCurrrens == usdOfLatin)
+                        {
+                            System.Console.WriteLine("Невозможно конвертировать");
+                        }else if(myCurrrens == smnOfLatin){
+                            myCurrrens = usdOfLatin;
+                            myBalance = myBalance/10.22;
+                            System.Console.WriteLine(myBalance+" "+myCurrrens);
+                        }else if(myCurrrens == eurOfLatin){
+                            myCurrrens = usdOfLatin;
+                            myBalance = myBalance*1.08;
+                            System.Console.WriteLine(myBalance+" "+myCurrrens);
+                        }else if(myCurrrens == rubOfLatin){
+                            myCurrrens = usdOfLatin;
+                            myBalance = myBalance/76.41;
+                            System.Console.WriteLine(myBalance+" "+myCurrrens);
+                        }
+                        break;
+
+                        case 3:
+                        if(myCurrrens == eurOfLatin){
+                            System.Console.WriteLine("Невозможно конвертировать");
+                        }else if(myCurrrens == usdOfLatin){
+                            myCurrrens = eurOfLatin;
+                            myBalance = myBalance/1.08;
+                            System.Console.WriteLine(myBalance+" "+myCurrrens);
+                        }else if(myCurrrens == smnOfLatin){
+                            myCurrrens = eurOfLatin;
+                            myBalance = myBalance/11.04;
+                            System.Console.WriteLine(myBalance+" "+myCurrrens);
+                        }else if(myCurrrens == rubOfLatin){
+                            myCurrrens = eurOfLatin;
+                            myBalance = myBalance/82.56;
+                            System.Console.WriteLine(myBalance+" "+myCurrrens);
+                        }
+                        break;
+
+                        case 4:
+
+                        break;
+
+                        case 5:
+                        check1 = false;
                         break;
                         
                     }
+                    }
                     break;
 
-                    case 2:
+                    case 3:
                     check = false;
                     break;
                     
@@ -56,16 +134,6 @@ namespace HW_04_04_task2
         
 
 
-    }
-
-    class CurrensSomoni{
-        public double smn{get;set;}
-        public string smnOfLatin = "Сомони";
-    
-    }
-    class CurrensUsd{
-        public double usd{get;set;}
-        public string usdOfLatin = "Доллар";
     }
 
     
