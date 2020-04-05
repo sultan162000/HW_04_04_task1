0ab700a2c20cdd056e94258e4cd3cc2f790fbb4e
Кодиров Султон <sultonkodiro109@gmail.com>
2020-04-05 14:16:05 +0500

    Added class Converter and method showMoney

--- Program.cs
+++ Program.cs
@@ -6,7 +6,30 @@ namespace HW_04_04_task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Converter con = new Converter(1000);
+            con.showMoney();
         }
+    }
+
+    class Converter{
+        public double smn{get;set;}
+        public double usd{get;set;}
+        public double eur{get;set;}
+        public double rub{get;set;}
+
+        public Converter(double somoni){
+            this.smn = somoni;
+            usd = smn/10.25;
+            eur = smn/11.04;
+            rub = smn/0.13;
+        }
+        public void showMoney(){
+            System.Console.WriteLine("Сомони: "+smn);
+            System.Console.WriteLine("Доллар: "+usd);
+            System.Console.WriteLine("Евро: "+eur);
+            System.Console.WriteLine("Рубль: "+rub);
+        }
+
+
     }
 }
